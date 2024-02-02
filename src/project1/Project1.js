2007961bdf44b51d4296f5774ee02b39f686522c
Anitha Reddy Pandem <anithaeee224@gmail.com>
2024-02-02 11:31:48 +0800

    first commit

---
+++ src/project1/Project1.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../project1/project1.css';
+import 'bootstrap/dist/css/bootstrap.css';
+const Project1 = () => {
+  return (
+    <div className='card'>
+      <div className='card-text'>
+        <h1>React Project</h1>
+      </div>
+
+    </div>
+  )
+}
+
+export default Project1;
